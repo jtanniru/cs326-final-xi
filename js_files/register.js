79adec10cf1f3814ecfb12f4b3ba65d2e9737910
jengyuauc <jengyu@controversies.info>
2020-11-07 17:31:20 -0500

    fixed functionality to match that of homepage.html, fixed incorrect source

--- js_files/register.js
+++ js_files/register.js
@@ -1,27 +1,37 @@
 'use strict';
 
-// get inputEmail, inputPassword, and inputName
-const emailValue = document.getElementById('createEmail').value; //or is it innerHTML?
-const passwordValue = document.getElementById('createPassword').value; // or is it innerHTML?
-const nameValue = document.getElementById('createName').value; // or is it innerHTML?
+console.log("register.js running");
 
 // action for createAccount
 const createAccountButton = document.getElementById('createAccount');
 createAccountButton.addEventListener('click', () => {
+
+  // get inputEmail, inputPassword, and inputName
+  const emailValue = document.getElementById('createEmail').value; 
+  const passwordValue = document.getElementById('createPassword').value; 
+  const nameValue = document.getElementById('createName').value; 
+  console.log(emailValue);
+  console.log(passwordValue);
+  console.log(nameValue);
+
   if(emailValue === '' || passwordValue === '' || nameValue === ''){
     alert('Requried sign in information is missing.');
   }
-  /*
-    look through the login table stored with either PostgreSQL or MongoDB
-    1)  for (all entries of the loginTable){
-          if (loginTable[email] === emailValue) {
-            // then this email already exists
-            alert('This email is already registered.');
+
+  else {
+    /*
+      look through the login table stored with either PostgreSQL or MongoDB
+      1)  for (all entries of the loginTable){
+            if (loginTable[email] === emailValue) {
+              // then this email already exists
+              alert('This email is already registered.');
+            }
+            else {
+              // this email is not registered yet and still able to be added to loginTable
+              // INSERT into database this information (not necessarily in this format): {email: 'email', password: 'password', name: 'name'}
+            }
           }
-          else {
-            // this email is not registered yet and still able to be added to loginTable
-            // INSERT into database this information (not necessarily in this format): {email: 'email', password: 'password', name: 'name'}
-          }
-        }
-  */
+    */
+    window.location.href = "courses.html";
+  }
 }); 
