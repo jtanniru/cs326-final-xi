d4aee367599b16ca954fbb54a6190dfa5795a82e
jengyuauc <jengyu@controversies.info>
2020-11-08 01:40:16 -0500

    adding listeners for checkboxes and create course button, need to implement function that displays the courses after they are created

--- js_files/courses.js
+++ js_files/courses.js
@@ -1,24 +1,79 @@
 'use strict';
 
-// get professor input, course name input
-const professorValue = document.getElementById('inputProfessor');
-const courseNameValue = document.getElementById('inputCourseName');
-
-// get div that contains list of checkboxes for course days input
-/*
-  for each checkbox Monday - Sunday, if the box is checked, then the course occurs on this day
-  the course days data can be saved like this {days: ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'etc.']}
-  that way, when days need to be compared, we can use array.includes('certain day')
-*/
-
-// action for create course button
-const createCourseButton = document.getElementById('coursesSubmit');
-createCourseButton.addEventListener('click', () => {
-  /*
-    if any of the inputs are empty, alert('Requried sign in information is missing.');
-
-    otherwise, create an object of the form described below and insert it to a courses table (?)
-  */
+// action: gives each checkbox an attribute when checked so the status of each can be easily observed later
+const monday = document.getElementById('monday');
+monday.addEventListener('click', () => {
+  if (monday.hasAttribute('checked')) {
+    monday.removeAttribute('checked');
+  }
 });
 
-// createCourseButton should create an object in the form of {courseSubject: 'course subject', courseProfessor: 'professor last name', courseName: 'course name like CICS305', courseDays: ['days']}
+const tuesday = document.getElementById('tuesday');
+tuesday.addEventListener('click', () => {
+  if (tuesady.hasAttribute('checked')) {
+    tuesday.removeAttribute('checked');
+  }
+});
+
+const wednesday = document.getElementById('wednesday');
+wednesday.addEventListener('click', () => {
+  if (wednesday.hasAttribute('checked')) {
+    wednesday.removeAttribute('checked');
+  }
+});
+
+const thursday = document.getElementById('thursday');
+thursday.addEventListener('click', () => {
+  if (thursday.hasAttribute('checked')) {
+    thursday.removeAttribute('checked');
+  }
+});
+
+const friday = document.getElementById('friday');
+friday.addEventListener('click', () => {
+  if (friday.hasAttribute('checked')) {
+    friday.removeAttribute('checked');
+  }
+});
+
+const saturday = document.getElementById('saturday');
+saturday.addEventListener('click', () => {
+  if (saturday.hasAttribute('checked')) {
+    saturday.removeAttribute('checked');
+  }
+});
+
+const sunday = document.getElementById('sunday');
+sunday.addEventListener('click', () => {
+  if (sunday.hasAttribute('checked')) {
+    sunday.removeAttribute('checked');
+  }
+});
+
+// action for submit course button
+const coursesSubmitButton = document.getElementById('coursesSubmit');
+coursesSubmitButton.addEventListener('click', () => {
+  console.log("clicked button");
+  const courseNameValue = document.getElementById('courseName');
+  const professorValue = document.getElementById('professor');
+  const weekDaysDiv = document.getElementById('courseDaysList');
+  let weekdayArray = [];
+  
+  // for checkbox in courseDays div, if a checkbox is checked, add its ID (the weekday) to the array
+  let weekDaysCheckboxes = weekDaysDiv.getElementsByTagName('input');
+  for (let i = 0; i < weekDaysCheckboxes.length; i++) {
+    if (weekDaysCheckboxes[i].hasAttribute('id')) {
+      console.log(weekDaysCheckboxes[i].id);
+      weekdayArray.push(weekDaysCheckboxes[i].id);
+    }
+  }
+  console.log(weekdayArray);  
+  
+  // database: insert an object with the information {email: 'unique email of user / or name?', courseName: 'course name', professor: 'professor', courseDays: [course days]}
+});
+
+// event listener on load that encompasses all of these other listeners
+
+// add in a way to create a new tab or div or something to store the text/info of the new course once it's created (think about when to delete them? or if they should be in a table, not tabs)
+// so get rid of the Add tab and make that a header instead. keep the rest of the gray div. make an empty table with just headers, then when a new course is added, add the course info to each
+// appropriate header/column and insert the data into the database (pg-promise if we use Heroku PostgreSQL)
