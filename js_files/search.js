9be66b3e1cd439d9f2ce5e0178548a7cebbd0b67
jengyuauc <jengyu@controversies.info>
2020-11-08 14:59:14 -0500

    so far just retreives the htmlelements for the 4 lists of checkboxes

---
+++ js_files/search.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// action: access checkboxes for each of the filter categories
+
+// for Courses
+const userCourseList = document.getElementById('listOfCourses');
+
+// for Professors
+const userProfessorList = document.getElementById('listOfProfessors');
+
+// for Course Days
+const userDaysList = document.getElementById('listOfDays');
+
+// for Timezones
+const userTimezoneList = document.getElementById('listOfTimezones');
