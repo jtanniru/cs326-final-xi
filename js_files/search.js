c4107cedbb99e779308405b949a31b7df974d086
jengyuauc <jengyu@controversies.info>
2020-11-15 22:42:32 -0500

    copying courses and search from chou-dev

---
+++ js_files/search.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// action: access checkboxes for each of the filter categories
+
+// for Courses
+const userCourseList = document.getElementById('listOfCourses');
+
+// for Professors
+const userProfessorList = document.getElementById('listOfProfessors');
+
+// for Course Days
+const userDaysList = document.getElementById('listOfDays');
+
+// for Timezones
+const userTimezoneList = document.getElementById('listOfTimezones');
+
+// all GET requests, no POSTS
+// once you do GET requests for each section, you can do SQL command to SELECT from table(s)
