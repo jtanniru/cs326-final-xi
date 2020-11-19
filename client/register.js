7ec939fbce2d19142f84f3ae57cfac48c467d711
jengyuauc <jengyu@controversies.info>
2020-11-18 21:44:09 -0500

    fixed blank innerHTML issue

--- client/register.js
+++ client/register.js
@@ -1,39 +1,39 @@
 'use strict';
 
 window.addEventListener("load", async function () {
-  // get inputEmail, inputPassword, and inputName
-  const emailValue = document.getElementById('createEmail').value; 
-  const passwordValue = document.getElementById('createPassword').value; 
-  const nameValue = document.getElementById('createName').value; 
-
   // action for createAccount
   const createAccountButton = document.getElementById('createAccount');
   createAccountButton.addEventListener('click', async () => {
 
+    // get inputEmail, inputPassword, and inputName
+    const emailValue = document.getElementById('createEmail').value; 
+    const passwordValue = document.getElementById('createPassword').value; 
+    const nameValue = document.getElementById('createName').value; 
+
     if(emailValue === '' || passwordValue === '' || nameValue === ''){
       alert('Required sign in information is missing.');
     }
-    // else {
-    //   // TODO: add header (THANKS OLIVIA!!)
-    //   const response = await fetch('/register', {
-    //     method: 'POST',
-    //     body: JSON.stringify({
-    //         email: emailValue ,
-    //         password: passwordValue,
-    //         name: nameValue
-    //     })
-    //   });
+    else {
+      // TODO: add header (THANKS OLIVIA!!)
+      const response = await fetch('/register', {
+        method: 'POST',
+        body: JSON.stringify({
+            email: emailValue ,
+            password: passwordValue,
+            name: nameValue
+        })
+      });
 
-    //   if (!response.ok) {
-    //     console.error("Could not save the user to the server.");
-    //   }
-    //   else{
-    //     console.log("user added.");
-    //   }
+      if (!response.ok) {
+        console.error("Could not save the user to the server.");
+      }
+      else{
+        console.log("user added.");
+      }
 
-    //   // // made email the primary key so I don't think i have to check for uniqueness when adding to the table. otherwise can be handled in server.
-    //   // window.location.href = "courses.html";
-    // }
+      // // made email the primary key so I don't think i have to check for uniqueness when adding to the table. otherwise can be handled in server.
+      // redirect to homepage.html (for login) (HALLOOO JANVI)
+    }
   });
 });
 
