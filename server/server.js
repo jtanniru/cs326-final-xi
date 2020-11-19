76fe8c288edace49ca1151d889758417ac5d2460
jengyuauc <jengyu@controversies.info>
2020-11-18 23:39:59 -0500

    adding server side

--- server/server.js
+++ server/server.js
@@ -1,16 +1,22 @@
 'use strict';
-// For loading environment variables.
-//require('dotenv').config();
 
-import config from 'dotenv';
-import express, { json, urlencoded, static } from 'express';                 // express routing
-import expressSession from 'express-session';  // for managing session state
-import { use, initialize, session as _session, serializeUser, deserializeUser, authenticate } from 'passport';               // handles authentication
-import { Strategy as LocalStrategy } from 'passport-local'; // username/password strategy
+const datafunc = require('./database.js');
+const path = require('path');
+
+// For loading environment variables.
+require('dotenv').config();
+
+const express = require('express');                 // express routing
+const expressSession = require('express-session');  // for managing session state
+const passport = require('passport');               // handles authentication
+const LocalStrategy = require('passport-local').Strategy; // username/password strategy
 const app = express();
 const port = process.env.PORT || 3000;
+const minicrypt = require('./miniCrypt.js');
 
-config();
+
+const mc = new minicrypt();
+
 // Session configuration
 
 const session = {
@@ -43,27 +49,37 @@ const strategy = new LocalStrategy(
 // App configuration
 
 app.use(expressSession(session));
-use(strategy);
-app.use(initialize());
-app.use(_session());
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static('client'));
+app.use(express.static('images'));
+
 
 // Convert user object to a unique identifier.
-serializeUser((user, done) => {
+passport.serializeUser((user, done) => {
     done(null, user);
 });
 // Convert a unique identifier to a user object.
-deserializeUser((uid, done) => {
+passport.deserializeUser((uid, done) => {
     done(null, uid);
 });
 
-app.use(json()); // allow JSON inputs
-app.use(urlencoded({'extended' : true})); // allow URLencoded data
+app.use(express.json()); // allow JSON inputs
+app.use(express.urlencoded({'extended' : true})); // allow URLencoded data
 
 /////
 
 // we use an in-memory "database"; this isn't persistent but is easy
-let users = { 'emery' : 'compsci326' } // default user
-let userMap = {};
+
+console.log(mc.hash('compsci326'));
+
+// let users = { 'emery' : 'compsci326' } // default user
+let users = { 'eberger@umass.edu' : [
+  '2401f90940e037305f71ffa15275fb0d',
+  '61236629f33285cbc73dc563cfc49e96a00396dc9e3a220d7cd5aad0fa2f3827d03d41d55cb2834042119e5f495fc3dc8ba3073429dd5a5a1430888e0d115250'
+] };
+
 
 // Returns true iff the user exists.
 function findUser(username) {
@@ -74,27 +90,26 @@ function findUser(username) {
     }
 }
 
+// TODO
 // Returns true iff the password is the one we have stored (in plaintext = bad but easy).
 function validatePassword(name, pwd) {
     if (!findUser(name)) {
 	return false;
-    }
-    if (users[name] !== pwd) {
-	return false;
-    }
-    return true;
+	}
+	const key = users[name];
+	const res = mc.check(pwd, key[0], key[1]);
+    return res;
 }
 
 // Add a user to the "database".
-// Return true if added, false otherwise (because it was already there).
 // TODO
 function addUser(name, pwd) {
-	// TODO
-	if(findUser(name)){
-		return false;
+    if (findUser(name)) {
+	return false;
 	}
 	else{
-		users[name] = pwd;
+		const [salt, hash] = mc.hash(pwd);
+		users[name] = [salt, hash];
 		return true;
 	}
 }
@@ -119,15 +134,14 @@ app.get('/',
 
 // Handle post data from the login.html form.
 app.post('/login',
-	 authenticate('local' , {     // use username/password authentication
+	 passport.authenticate('local' , {     // use username/password authentication
 	     'successRedirect' : '/private',   // when we login, go to /private 
 	     'failureRedirect' : '/login'      // otherwise, back to login
 	 }));
 
 // Handle the URL /login (just output the login.html file).
 app.get('/login',
-	(req, res) => res.sendFile('html/login.html',
-				   { 'root' : __dirname }));
+	(req, res) => res.sendFile(path.resolve('./client/homepage.html')));
 
 // Handle logging out (takes us back to the login page).
 app.get('/logout', (req, res) => {
@@ -136,31 +150,26 @@ app.get('/logout', (req, res) => {
 });
 
 
-// Add a new user and password IFF one doesn't exist already.
+// Like login, but add a new user and password IFF one doesn't exist already.
 // If we successfully add a new user, go to /login, else, back to /register.
 // Use req.body to access data (as in, req.body['username']).
 // Use res.redirect to change URLs.
-// TODO
 app.post('/register',
 	 (req, res) => {
 	     const username = req.body['username'];
 	     const password = req.body['password'];
-		 // Check if we successfully added the user.
-	     // If so, redirect to '/login'
-	     // If not, redirect to '/register'.
-		 if(addUser(username, password)){
-			res.redirect('/login');
-		 }
-		 else{
-			res.redirect('/register');
-		 }
-	     
+	     if (addUser(username, password)) {
+		 res.redirect('/login');
+	     } else {
+		 res.redirect('/register');
+	     }
 	 });
 
 // Register URL
 app.get('/register',
-	(req, res) => res.sendFile('html/register.html',
-				   { 'root' : __dirname }));
+	(req, res) => res.sendFile(path.resolve('./client/register.html')));
+	//res.sendFile('client/register.html',
+				   //{ 'root' : __dirname }));
 
 // Private data
 app.get('/private',
@@ -184,8 +193,6 @@ app.get('/private/:userID/',
 	    }
 	});
 
-app.use(static('html'));
-
 app.get('*', (req, res) => {
   res.send('Error');
 });
@@ -194,97 +201,40 @@ app.listen(port, () => {
     console.log(`App now listening at http://localhost:${port}`);
 });
 
-import * as utils from "./database.js";
-import pgp from "pg-promise";
-//const pgp = pgPromise({});
-import * as _express from "express";
-const express = _express["default"];
-
-// Local PostgreSQL credentials
-const username = "postgres";
-const password = "admin";
-
-const url = process.env.DATABASE_URL || `postgres://${username}:${password}@localhost/`;
-const db = pgp()(url);
-
-app.use('/', express.static('./client'));
-
-async function connectAndRun(task) {
-    let connection = null;
-
-    try {
-        connection = await db.connect();
-        return await task(connection);
-	} 
-	// eslint-disable-next-line no-useless-catch
-	catch (e) {
-        throw e;
-	} 
-	finally {
-        try {
-            connection.done();
-		} 
-		// eslint-disable-next-line no-empty
-		catch(ignored) {
-        }
-    }
-}
-
 // Three tables Users, Courses, Attend
 
-// EXPRESS SETUP
+// app.post('/register', async (req, res) => {
 
-const app = express();
+//     await datafunc.addUser(req.query.email, req.query.password);
+//     res.send("OK");
+// });
 
-//serving static files
-app.use(express.static('../js_files'));
-app.use(express.static('../HTML_CSS_files'));
+// app.get('/login', async (req, res) => {
+// 	const userInfo = await datafunc.getUser(req.query.email);
+//     res.send(JSON.stringify(userInfo));
+// });
 
-app.get('/', async (req, res) => {
-	await createCoursesTable();
-    await createUsersTable();
-    await createClassesTable();
-    res.send("OK");
-});
+// app.get('/course/id/new', async (req, res) => {
+//     await datafunc.addCourse(req.query.cid, req.query.course_title, req.query.course_subject, req.query.professor_name, req.query.course_number, req.query.course_days, req.query.course_time );
+//     res.send("OK");
+// });
 
-app.post('/register', async (req, res) => {
+// app.get('/course/id/delete', async (req, res) => {
+//     await datafunc.addCourse(req.query.cid, req.query.course_title, req.query.course_subject, req.query.professor_name, req.query.course_number, req.query.course_days, req.query.course_time );
+//     res.send("OK");
+// });
 
-    await utils.addUser(req.query.email, req.query.password);
-    res.send("OK");
-});
+// app.post('/user/id/settings/add', async function (req, res) {
+// 	const classes = await datafunc.getClasses();
+//     res.send(JSON.stringify(classes));
+//   });
 
-app.get('/login', async (req, res) => {
-    const userInfo = await utils.getUser(req.query.email);
-    res.send(JSON.stringify(userInfo));
-});
+// app.get("/user/id/settings/delete", async (req, res) => {
+//     await datafunc.deleteClasses(req.query.email, req.query.cid);
+//     res.send("OK");
+// });
 
-app.get('/course/id/new', async (req, res) => {
-    await utils.addCourse(req.query.cid, req.query.course_title, req.query.course_subject, req.query.professor_name, req.query.course_number, req.query.course_days, req.query.course_time );
-    res.send("OK");
-});
-
-app.get('/course/id/delete', async (req, res) => {
-    await utils.addCourse(req.query.cid, req.query.course_title, req.query.course_subject, req.query.professor_name, req.query.course_number, req.query.course_days, req.query.course_time );
-    res.send("OK");
-});
-
-app.post('/user/id/settings/add', async function (req, res) {
-	const classes = await utils.getClasses();
-    res.send(JSON.stringify(classes));
-  });
-
-app.get("/user/id/settings/delete", async (req, res) => {
-    await utils.deleteClasses(req.query.email, req.query.cid);
-    res.send("OK");
-});
-
-app.get('/user/id/search', async (req, res) => {
-    await utils.addClass(req.query.sid, req.query.cid, req.query.email);
-    res.send("OK");
-});
-
-app.get('*', (req, res) => {
-    res.send('NO FOOL');
-});
-
-app.listen(process.env.PORT || 8080);
+// app.get('/user/id/search', async (req, res) => {
+//     await datafunc.addClass(req.query.sid, req.query.cid, req.query.email);
+//     res.send("OK");
+// });
