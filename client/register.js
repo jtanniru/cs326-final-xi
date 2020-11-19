094312442218df7779763686412df067b1bd9bfa
jengyuauc <jengyu@controversies.info>
2020-11-18 21:37:21 -0500

    testing updates to register

--- client/register.js
+++ client/register.js
@@ -1,47 +1,39 @@
 'use strict';
-//import faker from 'faker';
 
-console.log("register.js running");
+window.addEventListener("load", async function () {
+  // get inputEmail, inputPassword, and inputName
+  const emailValue = document.getElementById('createEmail').value; 
+  const passwordValue = document.getElementById('createPassword').value; 
+  const nameValue = document.getElementById('createName').value; 
 
-// get inputEmail, inputPassword, and inputName
-const emailValue = document.getElementById('createEmail').value; 
-const passwordValue = document.getElementById('createPassword').value; 
-const nameValue = document.getElementById('createName').value; 
+  // action for createAccount
+  const createAccountButton = document.getElementById('createAccount');
+  createAccountButton.addEventListener('click', async () => {
 
-// action for createAccount
-const createAccountButton = document.getElementById('createAccount');
-createAccountButton.addEventListener('click', async () => {
-
-  if(emailValue === '' || passwordValue === '' || nameValue === ''){
-    alert('Required sign in information is missing.');
-  }
-  else {
-
-    const response = await fetch('/register', {
-      method: 'POST',
-      body: JSON.stringify({
-          email: emailValue ,
-          password: passwordValue,
-          name: nameValue
-      })
-    });
-
-    if (!response.ok) {
-      console.error("Could not save the user to the server.");
+    if(emailValue === '' || passwordValue === '' || nameValue === ''){
+      alert('Required sign in information is missing.');
     }
-    else{
-      console.log("user added.");
-    }
-    // made email the primary key so I don't think i have to check for uniqueness when adding to the table. otherwise can be handled in server.
-    window.location.href = "courses.html";
-  }
+    // else {
+    //   // TODO: add header (THANKS OLIVIA!!)
+    //   const response = await fetch('/register', {
+    //     method: 'POST',
+    //     body: JSON.stringify({
+    //         email: emailValue ,
+    //         password: passwordValue,
+    //         name: nameValue
+    //     })
+    //   });
+
+    //   if (!response.ok) {
+    //     console.error("Could not save the user to the server.");
+    //   }
+    //   else{
+    //     console.log("user added.");
+    //   }
+
+    //   // // made email the primary key so I don't think i have to check for uniqueness when adding to the table. otherwise can be handled in server.
+    //   // window.location.href = "courses.html";
+    // }
+  });
 });
-
-
- 
-//     var randomName = faker.name.findName(); // Rowan Nikolaus
-//     var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-
-
-
 
