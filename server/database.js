9706a4f5a3b93f491fcba06bd6746242ce669f29
jtanniru <jtanniru@users.noreply.github.com>
2020-11-22 03:00:28 -0500

    janvi database.js

--- server/database.js
+++ server/database.js
@@ -81,12 +81,15 @@ async function getUserSettings(email) {
 }
 
 async function searchUsers(course_names, professors, course_days, timezones) {
-    return await connectAndRun(db => db.any('SELECT name, email, phone FROM userInfo where (course_name IS NOT NULL = $1) and ($2 IS NOT NULL AND timezone = $2) and ($2 IS NOT NULL professor = $2) ;', [course_names, professors, course_days, timezones]));
+
+    return await connectAndRun(db => db.any('SELECT name, email, phone from userInfo join courseInfo on courseInfo.email = userInfo.email where course_name = $1 and ($1 IS NOT NULL AND timezone = $1)  ;', [course_names, professors, course_days, timezones]));
 }
 
 async function userAvailability(email) {
     return await connectAndRun(db => db.any('SELECT availability FROM userInfo where email = $1;', [email]));
 }
+
+//SELECT name, email, phone from userInfo join courseInfo on courseInfo.email = userInfo.email where course_name = 'CS 326';
 
 //db.any(SELECT * FROM ... WHERE ($1 IS NOT NULL AND timezone = $1) AND professor = "Berger", [data.timezone, data.professor])
 
